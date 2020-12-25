108f799af76c19a84f39e5e82252523668c39190
malikabakhtiyorova <malikabakhtiyorova@yandex.ru>
2020-12-24 17:00:14 -0800

    Finalize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {Switch, Route, NavLink} from 'react-router-dom'
 import './App.css';
+import Comments from './components/Comments/comments'
+import Posts from './components/Posts/posts'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavLink to='/'>Home</NavLink>
+     <NavLink to='/posts'>Posts</NavLink>
+     <NavLink to='/comments'>Comments</NavLink>
+
+     <Switch>
+       <Route path='/' exact/>
+       <Route path='/posts' component={Posts}  exact/>
+       <Route path='/comments/:id' component={Comments} exact/>
+     </Switch>
     </div>
   );
 }
 
 export default App;
+
